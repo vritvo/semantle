1def45c4e7a1508db6d44173ecebae47f7aad611
EthanJantz <21095848+EthanJantz@users.noreply.github.com>
2025-07-16 12:06:15 -0400

    Incorporate feedback from Solver #1

--- semantle/semantle.py
+++ semantle/semantle.py
@@ -1,84 +1,100 @@
 import time
 import os
 import gensim
-from gensim import utils
-import gensim.downloader as api
-from gensim.test.utils import common_texts
-from gensim.models import Word2Vec
 import csv
 
+ROOT_DIR = os.path.abspath(".")
+
 class MyCorpus:
-    '''An iterator that yields sentences (lists of str).'''
+    """An iterator that yields sentences (lists of str)."""
 
     def __iter__(self):
-        corpus_path = os.path.relpath('semantle/data/simpsons_script_lines.csv')
-        with open(corpus_path, newline='') as csvfile:
-            reader = csv.DictReader(csvfile, delimiter = ',', quotechar = '"')
+        corpus_path = ROOT_DIR + "/semantle/data/simpsons_script_lines.csv"
+        with open(corpus_path, newline="") as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=",", quotechar='"')
             for row in reader:
-                yield(row['normalized_text'].split())
+                yield (row["normalized_text"].split())
+
 
 class Semantle:
 
     def __init__(self):
-        # initialize model
         # TODO: Figure out how to use GoogleNews300 model
         # When loaded using gensim.downloader.load('word2vec-google-news-300') the model we get out is a KeyedVectors object, which doesn't work the same as a Word2Vec object
-        if not os.path.exists('semantle/data/simpsons.model'):
+        if not os.path.exists(ROOT_DIR + "/semantle/data/simpsons.model"):
             self.sentences = MyCorpus()
-            self.model = gensim.models.Word2Vec(sentences = self.sentences, vector_size=300, min_count=5)
-            self.model.save('semantle/data/simpsons.model')
+            self.model = gensim.models.Word2Vec(
+                sentences=self.sentences, vector_size=300, min_count=5
+            )
+            self.model.save(ROOT_DIR + "/semantle/data/simpsons.model")
         else:
-            self.model = gensim.models.Word2Vec.load('semantle/data/simpsons.model')
+            self.model = gensim.models.Word2Vec.load(
+                ROOT_DIR + "/semantle/data/simpsons.model"
+            )
 
         self.common_word_list = self.get_common_word_list()
-        self.word_of_the_day = 'bart'
+        self.word_of_the_day = "bart"
         self.guesses_dict = {}
         self.guesses_in_order = []
         self.endgame = False
-    
-    def get_common_word_list(self):
-        # load word list
-        path = 'semantle/data/popular.txt' # initially sourced from https://github.com/dolph/dictionary
+
+    def get_common_word_list(self) -> list[str]:
+        """
+        Reads in a list of the 5000 most popular words in the English
+        language to select for the word of the day.
+        """
+        path = "semantle/data/popular.txt"  # initially sourced from https://github.com/dolph/dictionary
         common_word_list = []
-        with open(path, 'r') as file:
+        with open(path, "r") as file:
             for line in file:
-                line = line.strip().strip()
+                line = line.strip()
                 common_word_list.append(line)
         return common_word_list
 
-    def player_guess(self):
-        '''
+    def player_guess(self) -> str:
+        """
         Handles the user input for a guess.
-        '''
+        """
         guess = input("Guess: ")
         cleaned_guess = guess.lower()
         return cleaned_guess
 
-    def take_turn(self):
-        '''
-        The core turn structure of the game.
-        '''
-        taking_guess = True
+    def take_turn(self) -> None:
+        """
+        Takes in the user's guess, updates the game log, and gives the
+        player feedback on their guess.
+        """
+        similarity_of_current_guess = None
 
-        while taking_guess:     
+        while similarity_of_current_guess is None:
             current_guess = self.player_guess()
-            taking_guess, similarity_of_current_guess = self.check_guess(current_guess)
+            similarity_of_current_guess = self.check_guess(current_guess)
 
         self.guesses_dict[current_guess] = similarity_of_current_guess
         self.guesses_in_order.append(current_guess)
         print(f"{current_guess}: {similarity_of_current_guess}")
         self.update_game_state(current_guess)
-    
-    def check_guess(self, guess):
+
+    def check_guess(self, guess) -> float | None:
+        """
+        Calculates the similarity of the current guess if the guessed word
+        is in the vocabulary of the game model.
+        """
         try:
-            similarity_of_current_guess = self.model.wv.similarity(guess, self.word_of_the_day)
-            return False, similarity_of_current_guess
+            similarity_of_current_guess = self.model.wv.similarity(
+                guess, self.word_of_the_day
+            )
+            return similarity_of_current_guess
         except:
-            return True, None
-    
-    def update_game_state(self, current_guess):
+            return None
+
+    def update_game_state(self, current_guess) -> None:
+        """
+        Sets the game state to end if the user guesses the correct word.
+        """
         self.endgame = current_guess == self.word_of_the_day
-    
+
+
 if __name__ == "__main__":
     print("loading...")
     ct = time.time()
--- semantle/solver.py
+++ semantle/solver.py
@@ -1,28 +1,43 @@
-#!/usr/bin/env python3
-
 from semantle import Semantle
-from gensim.models import KeyedVectors
+
 
 class Solver:
 
     def __init__(self, semantle: Semantle):
         self.semantle = semantle
         self.wv = semantle.model.wv
-        self.guesses = {'elementary': {}, 'green': {}, 'volume': {}}
-        self.rounding_value = '.2f'
-        
-    def get_similarity_score(self, guessed_word):
-        _, self.similarity_score = self.semantle.check_guess(guessed_word)
-    
-    def generate_vocab_distance_table(self, guessed_word):
+        self.guesses = {"elementary": {}, "green": {}, "volume": {}}
+        self.rounding_value = ".2f"
+
+    def get_similarity_score(self, guessed_word: str) -> None:
+        """
+        Assigns the similarity score of a given guess to a variable.
+        """
+        self.similarity_score = self.semantle.check_guess(guessed_word)
+
+    def generate_vocab_distance_table(self, guessed_word: str) -> dict[str, str]:
+        """
+        Calculates the distances of a given word to every other word in the model vocabulary.
+        """
         vocab = self.wv.key_to_index
         distances = self.wv.distances(guessed_word)
-        vocab_distances = {v: format(d, self.rounding_value) for v, d in zip(vocab, distances)}
+        assert len(vocab) == len(
+            distances
+        ), "vocab and vocab distances should be of equal length"
+        vocab_distances = {
+            v: format(d, self.rounding_value) for v, d in zip(vocab, distances)
+        }
 
         return vocab_distances
 
-    def collect_potential_answers(self, guessed_word, vocab_distances):
-        target_similarity_score = format(1 - self.similarity_score, '.2f')
+    def collect_potential_answers(
+        self, guessed_word: str, vocab_distances: dict[str, str]
+    ) -> list[str]:
+        """
+        Calculates the similarity score of the word of the day two a two point precision
+        and filters the vocab_distances table down to words with that value
+        """
+        target_similarity_score = format(1 - self.similarity_score, ".2f")
         print(target_similarity_score)
         potential_answers = []
         for key in vocab_distances:
@@ -30,23 +45,29 @@ class Solver:
                 potential_answers.append(key)
         return potential_answers
 
-    def triangulate(self):
-        for guess in self.guesses.keys():
-            similarity_score = self.get_similarity_score(guess)
+    def create_answer_table(self) -> None:
+        """
+        Creates the table of potential answers for each guess.
+        """
+        for guess in self.guesses:
+            self.get_similarity_score(guess)
             vocab_distances = self.generate_vocab_distance_table(guess)
             potential_answers = self.collect_potential_answers(guess, vocab_distances)
             for potential_answer in potential_answers:
                 self.guesses[guess][potential_answer] = True
 
-    def identify_solution(self):
+    def identify_solution(self) -> str:
+        """
+        Identifies the one word shared between all of the potential_answers tables for each guess
+        """
         keys = list(self.guesses.keys())
-        for word in list(self.guesses[keys[0]].keys()):
+        for word in list(self.guesses[keys[0]]):
             if self.guesses[keys[1]].get(word) and self.guesses[keys[2]].get(word):
                 return word
 
+
 if __name__ == "__main__":
     s = Solver(Semantle())
-    s.triangulate()
+    s.create_answer_table()
     solution = s.identify_solution()
     print(f"{solution} is the solution to today's Semantle")
-
